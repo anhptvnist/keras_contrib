391276bda6b7ba20909a1dc27988f8b98771464f
anhptvnist <anhpt.vnist@gmail.com>
2020-12-30 17:44:48 +0700

    keras

--- keras_contrib/layers/crf.py
+++ keras_contrib/layers/crf.py
@@ -460,7 +460,8 @@ class CRF(Layer):
             if K.backend() == 'theano':
                 m = states[3][:, t:(t + 2)]
             else:
-                m = K.slice(states[3], [0, t], [-1, 2])
+                import tensorflow as tf  # modified K.slice to tf.slice, keras=2.1.4, keras_contrib=2.0.8, tensorflow=1.0.0, python=3.6
+                m = tf.slice(states[3], [0, t], [-1, 2])
             input_energy_t = input_energy_t * K.expand_dims(m[:, 0])
             # (1, F, F)*(B, 1, 1) -> (B, F, F)
             chain_energy = chain_energy * K.expand_dims(
